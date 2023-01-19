52576e5097829ebd6e8baa3646e993a02d1c7e15
andiniz122 <andiniz122@gmail.com>
2023-01-18 21:10:16 -0300

    correção

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3000';
-// axios.defaults.baseURL = 'https://api.eletricom.me'; // teste
+// axios.defaults.baseURL = 'https://api.eletricom.me'; 
 
 axios.interceptors.request.use(function (config) {
 	let loginData = JSON.parse(localStorage.getItem('login-data'));
